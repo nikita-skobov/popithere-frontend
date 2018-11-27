2cefb548d5258a2cbb9be0d923e0f1f7590671e0
equilateral <equilateralllc@gmail.com>
2018-11-27 13:45:08 -0600

    adds helper functions for resizing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,81 @@
+/* global window */
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+
+import { Container, Row, Col } from 'reactstrap'
 
 export default class App extends Component {
   constructor(props) {
     super(props)
-    this.name = props.name
+    this.brain = props.brain
+    this.sideBarMinWidth = props.sideBarMinWidth
+    this.sideBarPercent = props.sideBarPercent
+
+    const iw = window.innerWidth
+    const ih = window.innerHeight
+
+    this.state = {
+      orientation: iw > ih ? 'landscape' : 'portrait',
+    }
+
+    this.brain.store('App', this)
+
+    this.shouldResize = this.shouldResize.bind(this)
+  }
+
+  shouldResize(iw, ih) {
+    // inner width, inner height
+    const { orientation } = this.state
+    let should = false
+    let newOr = orientation
+    if (orientation === 'landscape' && iw <= ih) {
+      newOr = 'portrait'
+      should = true
+    } else if (orientation === 'portrait' && ih <= iw) {
+      newOr = 'landscape'
+      should = true
+    }
+    if (newOr !== orientation) {
+      this.setState({ orientation: newOr })
+    }
+    return should
   }
 
   render() {
-    return (
-      <div className="App">
-        <h2>hello {this.name}</h2>
-      </div>
-    )
+    const { orientation } = this.state
+    console.log('rendering app')
+    if (orientation === 'landscape') {
+      // render for desktop
+      return (
+        <div className="parent">
+          <div className="square-holder" />
+          <div className="sidebar" />
+        </div>
+      )
+    }
+
+    return <div />
+    // else, render for mobile/tablet
+
+    // return (
+    //   <Container fluid>
+    //     <Row>
+    //       <Col xs="5"> this 5 ay</Col>
+    //       <Col xs="auto">
+    //         <canvas width="50vw" height="50vw" />
+    //       </Col>
+    //     </Row>
+    //   </Container>
+    // )
   }
 }
 
 App.defaultProps = {
-  name: null,
+  sideBarMinWidth: 300,
 }
 
 App.propTypes = {
-  name: PropTypes.string,
+  brain: PropTypes.instanceOf(Object).isRequired,
+  sideBarMinWidth: PropTypes.number,
+  sideBarPercent: PropTypes.number.isRequired,
 }
