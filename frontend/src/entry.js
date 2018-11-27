1683b93dc113e07e2422db3c4fbfe5980f6da530
equilateral <equilateralllc@gmail.com>
2018-11-27 13:10:55 -0600

    adds brain and resize listener

--- frontend/src/entry.js
+++ frontend/src/entry.js
@@ -1,4 +1,4 @@
-/* global document */
+/* global document window */
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
@@ -6,4 +6,25 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 const reactContainer = document.getElementById('react-container')
 
-ReactDOM.render(<App />, reactContainer)
+const brain = (function brain() {
+  const components = {}
+
+  return {
+    store: (name, ref) => { components[name] = ref },
+    tell: components,
+    ask: components,
+  }
+}())
+
+window.addEventListener('resize', (e) => {
+  try {
+    const ih = window.innerHeight
+    const iw = window.innerWidth
+    brain.ask.App.shouldResize(iw, ih)
+  } catch (err) {
+    // do nothing, try again
+    // on next resize event
+  }
+})
+
+ReactDOM.render(<App brain={brain} />, reactContainer)
