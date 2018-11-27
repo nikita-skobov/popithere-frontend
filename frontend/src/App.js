6eee4889fc600df37ad2280e76b94414f4b503c1
equilateral <equilateralllc@gmail.com>
2018-11-27 17:31:52 -0600

    adds prop to buttons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -39,7 +39,7 @@ export default class App extends Component {
   render() {
     return [
       <Canvas brain={this.brain} />,
-      <Buttons brain={this.brain} />,
+      <Buttons buttonsWidth={80} brain={this.brain} />,
     ]
   }
 }
