aab722edc7bf437769522238e84eeabe7dcb9c49
equilateral <equilateralllc@gmail.com>
2018-11-27 10:28:45 -0600

    adds bootstrap css as import

--- frontend/src/entry.js
+++ frontend/src/entry.js
@@ -2,6 +2,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const reactContainer = document.getElementById('react-container')
 
