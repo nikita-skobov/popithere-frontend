3b132d0069f0a862f8863e9c72f2a2767ffd72d6
equilateral <equilateralllc@gmail.com>
2018-11-27 20:41:23 -0600

    adds chat rendering for each message

--- frontend/src/components/ChatBox.js
+++ frontend/src/components/ChatBox.js
@@ -7,14 +7,27 @@ export default class ChatBox extends Component {
     super(props)
     this.brain = props.brain
 
+    this.state = {
+      chats: [
+        {
+          name: 'olive oil',
+          msg: 'asdsadsadsadsadsa',
+        },
+      ],
+    }
+
     this.brain.store('ChatBox', this)
   }
 
 
   render() {
+    const { chats } = this.state
     return (
       <div className="input-margins chat-box">
-        fdsafdas
+        {chats.reverse().map((item) => {
+          const { name, msg } = item
+          return <div> <span>{name}</span>: {msg} </div>
+        })}
       </div>
     )
   }
