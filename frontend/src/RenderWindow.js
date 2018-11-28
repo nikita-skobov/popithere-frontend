707e38f0bd9d2b3e14d1b20bd4dbde87c4702f12
nskobov <33850203+nskobov@users.noreply.github.com>
2018-11-28 13:57:40 -0600

    removes old console logs/comments

--- frontend/src/RenderWindow.js
+++ frontend/src/RenderWindow.js
@@ -23,23 +23,11 @@ export default class RenderWindow {
     const oldCanvas = document.getElementsByTagName('canvas')[0]
     this.renderer.view.classList.add('canvas')
     oldCanvas.replaceWith(this.renderer.view)
-
-    // const setup = () => {
-    //   console.log('doing setup!')
-    //   const cat = new PIXI.Sprite(PIXI.loader.resources.test1.texture)
-    //   this.stage.addChild(cat)
-    //   this.renderer.render(this.stage)
-    // }
-
-    // PIXI.loader
-    //   .add('test1', 'images/test1.png')
-    //   .load(setup)
   }
 
   drawImage(name, { x, y }) {
     console.log(`drawing ${name}`)
     console.log(x, y)
-    console.log(`Clearing before render? ${this.renderer.clearBeforeRender}`)
     const cat = new PIXI.Sprite(PIXI.loader.resources[name].texture)
     cat.x = x
     cat.y = y
