d14662e441cef1d60743e0f346f0d8b1fb2e9c80
equilateral <equilateralllc@gmail.com>
2018-11-27 20:49:49 -0600

    adds addChat function, fixes chats reverse map
    if you dont use slice(0) before reverse it mutates the array

--- frontend/src/components/ChatBox.js
+++ frontend/src/components/ChatBox.js
@@ -17,14 +17,23 @@ export default class ChatBox extends Component {
     }
 
     this.brain.store('ChatBox', this)
+    this.addChat = this.addChat.bind(this)
   }
 
+  addChat(chat) {
+    console.log(this.state.chats[0])
+    console.log(this.state.chats)
+    this.setState((prevState) => {
+      prevState.chats.push(chat)
+      return prevState
+    })
+  }
 
   render() {
     const { chats } = this.state
     return (
       <div className="input-margins chat-box">
-        {chats.reverse().map((item) => {
+        {chats.slice(0).reverse().map((item) => {
           const { name, msg } = item
           return <div> <span>{name}</span>: {msg} </div>
         })}
