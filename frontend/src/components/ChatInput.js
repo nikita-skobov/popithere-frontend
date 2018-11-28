68bfd42f481c0459b663ea0c5d971b1aa0bc4e6e
equilateral <equilateralllc@gmail.com>
2018-11-27 20:23:03 -0600

    adds form submit

--- frontend/src/components/ChatInput.js
+++ frontend/src/components/ChatInput.js
@@ -10,16 +10,28 @@ export default class ChatInput extends Component {
     this.brain = props.brain
 
     this.brain.store('ChatInput', this)
+
+    this.onSubmit = this.onSubmit.bind(this)
+  }
+
+  onSubmit(e) {
+    e.preventDefault()
+    document.activeElement.blur()
+    const input = e.target.getElementsByTagName('input')[0]
+    const value = input.value
+    input.value = ""
   }
 
   render() {
     return (
-      <InputGroup className="input-margins">
-        <Input placeholder="Send a message" />
-        <InputGroupAddon addonType="append">
-          <Button>Chat</Button>
-        </InputGroupAddon>
-      </InputGroup>
+      <form action="#" onSubmit={this.onSubmit}>
+        <InputGroup className="input-margins">
+          <Input placeholder="Send a message" />
+          <InputGroupAddon addonType="append">
+            <Button>Chat</Button>
+          </InputGroupAddon>
+        </InputGroup>
+      </form>
     )
   }
 }
