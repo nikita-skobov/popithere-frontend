9052b7a9fe9a4711250edd87cad9858310be2528
equilateral <equilateralllc@gmail.com>
2018-11-27 22:23:26 -0600

    reorder imports

--- frontend/src/entry.js
+++ frontend/src/entry.js
@@ -1,8 +1,9 @@
 /* global document window */
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
 import 'bootstrap/dist/css/bootstrap.min.css'
+
+import App from './App'
 
 const reactContainer = document.getElementById('react-container')
 
