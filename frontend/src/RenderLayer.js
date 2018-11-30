8c5c67087036cf18ee0f4d5851450d892ea3c8df
nskobov <33850203+nskobov@users.noreply.github.com>
2018-11-30 12:16:29 -0600

    adds basic constructor, interactivity option

--- frontend/src/RenderLayer.js
+++ frontend/src/RenderLayer.js
@@ -2,6 +2,42 @@ import * as PIXI from 'pixi.js'
 
 export default class RenderLayer {
   constructor(props) {
-    this.layer = new PIXI.Container()
+    // size: this.size,
+    // interactive: false, // default is false
+    // backgroundColor: props.backgroundColor, // default is white,
+    // transparent: false, // default is false
+    this.size = props.size
+
+    this.renderer = PIXI.autoDetectRenderer(
+      this.size[0],
+      this.size[1],
+      {
+        preserveDrawinngBuffer: false,
+        transparent: props.transparent || false,
+      },
+    )
+
+    if (!props.transparent) {
+      this.renderer.backgroundColor = props.backgroundColor || 0xffffff
+    }
+
+    // by default, no interaction
+    this.inputBox = null
+
+    if (props.interactive) {
+      this.makeInteractive()
+    }
+  }
+
+  makeInteractive() {
+    this.inputBox = new PIXI.Container()
+    this.inputBox.interactive = true
+    this.inputBox.hitArea = new PIXI.Rectangle(0, 0, this.size[0], this.size[1])
+    this.inputBox.on('pointermove', (event) => {
+      this.onPointerMove(event)
+    })
+    this.inputBox.on('pointerdown', (event) => {
+      this.onPointerDown(event)
+    })
   }
 }
