9c652c4500ef1e2ae1f9ba4922db05fe6389e426
nskobov <33850203+nskobov@users.noreply.github.com>
2018-11-30 13:41:50 -0600

    adds button properties and methods to Game class

--- frontend/src/Games/Game.js
+++ frontend/src/Games/Game.js
@@ -1,8 +1,47 @@
+const has = Object.prototype.hasOwnProperty
+
 export default class Game {
   constructor(props) {
     console.log('inside game cosntructor')
     console.log(props)
     this.baseLayer = props.baseLayer
     this.inputLayer = props.inputLayer
+
+    this.buttons = []
+  }
+
+  getButtons() {
+    return this.buttons
+  }
+
+
+  addButton(btn) {
+    // can either be an obj or string
+    if (typeof btn === 'string') {
+      // if its string then the button name, and text are the same
+      this.buttons.push({
+        name: btn,
+        text: btn,
+      })
+    } else if (typeof btn === 'object') {
+      if (!has.call(btn, 'name')) {
+        throw new Error('addButton object must contain a name property')
+      }
+      if (!has.call(btn, 'text')) {
+        throw new Error('addButton object must contain a text property')
+      }
+      if (typeof btn.name !== 'string') {
+        throw new Error('name must be a string')
+      }
+      if (typeof btn.text !== 'string') {
+        throw new Error('text must be a string')
+      }
+      this.buttons.push({
+        name: btn.name,
+        text: btn.text,
+      })
+    } else {
+      throw new Error('addButton argument must be either a string or object')
+    }
   }
 }
