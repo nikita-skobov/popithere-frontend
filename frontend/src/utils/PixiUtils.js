d01f2188a0b0f7fc152f84e92e481ac37ea3b691
equilateral <equilateralllc@gmail.com>
2018-12-19 10:36:51 -0600

    adds background color as option

--- frontend/src/utils/PixiUtils.js
+++ frontend/src/utils/PixiUtils.js
@@ -6,6 +6,7 @@ export function createRenderer({
   size,
   preserveDrawingBuffer,
   transparent,
+  backgroundColor,
 }) {
   const renderer = PIXI.autoDetectRenderer(
     size[0],
@@ -13,6 +14,7 @@ export function createRenderer({
     {
       preserveDrawingBuffer,
       transparent,
+      backgroundColor,
     },
   )
 
