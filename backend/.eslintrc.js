4edf1a12819cee2b2b5ce1e907efbb494f21216d
equilateral <equilateralllc@gmail.com>
2018-12-22 12:27:25 -0600

    deletes backend code

--- backend/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    "extends": "airbnb-base",
-    "rules": {
-        "semi": [2, "never"],
-        "linebreak-style": ["off"],
-        "no-console": ["off"],
-        "arrow-body-style": ["off", "as-needed"],
-    },
-};
