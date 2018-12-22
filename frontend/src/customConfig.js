e08834be78068a090e667e7ead250c3ed528b22b
equilateral <equilateralllc@gmail.com>
2018-12-22 12:01:30 -0600

    removes duplicate images from customConfig

--- frontend/src/customConfig.js
+++ frontend/src/customConfig.js
@@ -1,103 +1,19 @@
 exports.assetList = [
   {
     name: 'P_rotate',
-    url: 'images/rotateBetter.png',
+    url: 'images/temp/rotateBetter.png',
   },
   {
     name: '8panels',
-    url: 'images/8panels.png',
+    url: 'images/temp/8panels.png',
   },
   {
     name: 'test2',
-    url: 'images/test1.png',
+    url: 'images/temp/test1.png',
   },
   {
     name: 'test1',
-    url: 'images/hqdefault.jpg',
-  },
-  {
-    name: '8panels2',
-    url: 'images/8panels.png',
-  },
-  {
-    name: 'test22',
-    url: 'images/test1.png',
-  },
-  {
-    name: 'test12',
-    url: 'images/hqdefault.jpg',
-  },
-  {
-    name: '8panels3',
-    url: 'images/8panels.png',
-  },
-  {
-    name: 'test23',
-    url: 'images/test1.png',
-  },
-  {
-    name: 'test13',
-    url: 'images/hqdefault.jpg',
-  },
-  {
-    name: '8panel4s',
-    url: 'images/8panels.png',
-  },
-  {
-    name: 'test24',
-    url: 'images/test1.png',
-  },
-  {
-    name: 'test14',
-    url: 'images/hqdefault.jpg',
-  },
-  {
-    name: '8panels5',
-    url: 'images/8panels.png',
-  },
-  {
-    name: 'test25',
-    url: 'images/test1.png',
-  },
-  {
-    name: 'test15',
-    url: 'images/hqdefault.jpg',
-  },
-  {
-    name: '8panels6',
-    url: 'images/8panels.png',
-  },
-  {
-    name: 'test27',
-    url: 'images/test1.png',
-  },
-  {
-    name: 'test16',
-    url: 'images/hqdefault.jpg',
-  },
-  {
-    name: '8panels8',
-    url: 'images/8panels.png',
-  },
-  {
-    name: 'test28',
-    url: 'images/test1.png',
-  },
-  {
-    name: 'test18',
-    url: 'images/hqdefault.jpg',
-  },
-  {
-    name: '8panels9',
-    url: 'images/8panels.png',
-  },
-  {
-    name: 'test29',
-    url: 'images/test1.png',
-  },
-  {
-    name: 'test19',
-    url: 'images/hqdefault.jpg',
+    url: 'images/temp/hqdefault.jpg',
   },
 ]
 
