1ff3a025edf05f610adb7c556ecf46772c25f8d6
nikita-skobov <nskobov.websites@gmail.com>
2018-12-25 11:00:06 -0600

    adds sprite generation, and color alternation

--- frontend/src/components/ChatBox.js
+++ frontend/src/components/ChatBox.js
@@ -10,12 +10,36 @@ const avatars1 = new Avatars(SpriteCollection1)
 const avatars2 = new Avatars(SpriteCollection2)
 const avatars3 = new Avatars(SpriteCollection3)
 
+const has = Object.prototype.hasOwnProperty
+
+function generateSprite(author) {
+  const isCapital = /^[A-Z]/.test(author)
+  const isLowerCase = /^[a-z]/.test(author)
+  let svg = null
+  if (isCapital) {
+    // use male avatar
+    svg = avatars2.create(author)
+  } else if (isLowerCase) {
+    // use female avatar
+    svg = avatars3.create(author)
+  } else {
+    // use identicon avatar
+    svg = avatars1.create(author)
+  }
+  let svgstr = encodeURIComponent(svg)
+  const dataUri = `data:image/svg+xml,${svgstr}`
+  svgstr = null
+  svg = null
+  return dataUri
+}
+
 export default class ChatBox extends Component {
   constructor(props) {
     super(props)
     this.brain = props.brain
 
     this.state = {
+      colorIndex: 0,
       maxHeight: 300,
       chats: [
         {
@@ -30,14 +54,10 @@ export default class ChatBox extends Component {
 
     this.socket = this.brain.ask.Sockets
     if (this.socket.isConnected()) {
-      this.socket.on('co', (chatObj) => {
-        const newChatObj = {
-          name: chatObj.i,
-          msg: chatObj.t,
-        }
-        this.addChat(newChatObj)
-      })
+      this.socket.on('co', this.addChat)
     }
+
+    this.authors = {}
   }
 
   componentDidMount() {
@@ -47,14 +67,40 @@ export default class ChatBox extends Component {
 
   componentWillUnmount() {
     if (this.socket.isConnected()) {
-      this.socket.off('ci')
+      this.socket.off('ci', this.addChat)
     }
   }
 
   addChat(chat) {
     this.setState((prevState) => {
-      prevState.chats.push(chat)
-      return prevState
+      const tempState = prevState
+      const { colorIndex } = tempState
+      const name = chat.i
+      const msg = chat.t
+      let svg = ''
+      if (has.call(this.authors, name)) {
+        // we already generated a sprite, so use it
+        svg = this.authors[name]
+      } else {
+        // generate a new sprite
+        svg = generateSprite(name)
+        this.authors[name] = svg
+      }
+      let color = 'white'
+      if (colorIndex) {
+        color = '#e4e4e4'
+        tempState.colorIndex = 0
+      } else {
+        tempState.colorIndex = 1
+      }
+      const newChat = {
+        name,
+        msg,
+        svg,
+        color,
+      }
+      tempState.chats.push(newChat)
+      return tempState
     })
   }
 
