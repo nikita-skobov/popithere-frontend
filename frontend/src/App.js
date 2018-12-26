752cd957c22afe23a6d6f2bd69e9787156c22fdb
nikita-skobov <nskobov.websites@gmail.com>
2018-12-25 20:26:38 -0600

    connected is no longer needed. replaced by ready

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -27,7 +27,6 @@ export default class App extends Component {
 
     this.state = {
       loggedIn: token && !tokenManager.isTokenExpired(),
-      connected: this.brain.ask.Sockets.isConnected(),
       ready: false,
     }
 
@@ -86,7 +85,6 @@ export default class App extends Component {
     this.setState({ loggedIn: true })
 
     this.brain.tell.Sockets.connect(token, (socket) => {
-      this.setState({ connected: true })
       console.log('connected')
       socket.emit('sni', '')
       socket.on('sno', (sn) => {
