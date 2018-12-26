fd90623d7694f44741d702c0cd22d5327018b243
nikita-skobov <nskobov.websites@gmail.com>
2018-12-25 19:56:59 -0600

    makes addMessage more robust

--- frontend/src/components/Welcome.js
+++ frontend/src/components/Welcome.js
@@ -9,6 +9,7 @@ export default class Welcome extends Component {
     this.state = {
       messages: [props.initialMessage],
       warnings: [],
+      errors: [],
       ready: true,
     }
 
@@ -16,23 +17,24 @@ export default class Welcome extends Component {
     this.addMessage = this.addMessage.bind(this)
   }
 
-  addMessage(msg) {
+  addMessage(msg, notReady) {
     if (typeof msg === 'string') {
-      // if its just a string, add to messages, dont affect ready
+      // if its just a string, add to messages
       this.setState((prevState) => {
         const tempState = prevState
         tempState.messages.push(msg)
+        if (notReady) tempState.ready = false
         return tempState
       })
     } else {
       // otherwise its an object
-      const str = msg.msg
-      const { warning } = msg
+      const { warning, error, message } = msg
       this.setState((prevState) => {
         const tempState = prevState
-        tempState.warnings.push(warning)
-        tempState.messages.push(str)
-        tempState.ready = false
+        if (warning) tempState.warnings.push(warning)
+        if (error) tempState.errors.push(error)
+        if (message) tempState.messages.push(message)
+        if (notReady) tempState.ready = false
         return tempState
       })
     }
