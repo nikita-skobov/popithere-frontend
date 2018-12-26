384cbfb548376505d0075568c67f6e2ceafe4ce8
nikita-skobov <nskobov.websites@gmail.com>
2018-12-25 19:51:04 -0600

    adds addMessage method

--- frontend/src/components/Welcome.js
+++ frontend/src/components/Welcome.js
@@ -8,9 +8,34 @@ export default class Welcome extends Component {
 
     this.state = {
       messages: [props.initialMessage],
+      warnings: [],
+      ready: true,
     }
 
     this.brain.store('Welcome', this)
+    this.addMessage = this.addMessage.bind(this)
+  }
+
+  addMessage(msg) {
+    if (typeof msg === 'string') {
+      // if its just a string, add to messages, dont affect ready
+      this.setState((prevState) => {
+        const tempState = prevState
+        tempState.messages.push(msg)
+        return tempState
+      })
+    } else {
+      // otherwise its an object
+      const str = msg.msg
+      const { warning } = msg
+      this.setState((prevState) => {
+        const tempState = prevState
+        tempState.warnings.push(warning)
+        tempState.messages.push(str)
+        tempState.ready = false
+        return tempState
+      })
+    }
   }
 
   render() {
