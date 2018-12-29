e6d3fd111333cd51fabc521475bee3b102a77b44
nikita-skobov <nskobov.websites@gmail.com>
2018-12-29 00:30:57 -0600

    makes fetchData reusable for 2 types of fetch calls
    it cann either be called by iterating through the dataList, or it can be called with an s3 key and datanummber

--- frontend/src/utils/DataManager.js
+++ frontend/src/utils/DataManager.js
@@ -22,6 +22,9 @@ function DataManager(datastore) {
   // where to download that data nummber from
   let dataList = []
 
+  // a map of data numbers to s3 keys. useful for getDataLater
+  const s3keys = {}
+
   // this is an in memory map of data that the client currently
   // has. data can (AND SHOULD!) be released when it is no longer needed
   // the keys in this object are the  dataNumbers, and the value is whatever
@@ -39,13 +42,34 @@ function DataManager(datastore) {
   let dataNumberList = []
 
   const retObj = {
-    fetchData: (index) => {
+    fetchData: (index, obj) => {
+      let s3key
+      let dataNumber
+      let cb = false
+      if (typeof index === 'number') {
+        if (index > dataList.length) throw new Error('Fetch index out of range')
+        s3key = dataList[index].si
+        dataNumber = dataList[index].dn
+      }
+      if (obj) {
+        // case of 2nd parameter is an object.
+        // it is implied that if key is provided, so is dn
+        s3key = obj.key
+        dataNumber = obj.dn
+        // eslint-disable-next-line
+        cb = obj.cb
+      }
       try {
-        if (index > dataList.length) throw new Error('Fetch index out of range!')
-
-        const s3key = dataList[index].si
-        const dataNumber = dataList[index].dn
-        fetchingMap[dataNumber] = false
+        if (cb) {
+          // if user provided callback, set fetch map
+          // to an array of callbacks
+          fetchingMap[dataNumber] = [cb]
+        } else {
+          // otherwise set it to false. this way
+          // if there are future hooks, theyll see that it is false,
+          // and they can change it to an array of callbacks
+          fetchingMap[dataNumber] = false
+        }
         // this gets the s3 object key
         // which is appended to the api base
         // and then fetched
@@ -53,7 +77,7 @@ function DataManager(datastore) {
         console.log(url)
         fetch(url)
           .then(resp => resp.json())
-          .then((obj) => {
+          .then((obj2) => {
             console.log(`got ${s3key}, dn: ${dataNumber}`)
             dataObj[dataNumber] = obj
 
@@ -62,10 +86,15 @@ function DataManager(datastore) {
               // getDataLater. if fetchingMap
               // of dataNumber is false, that means
               // no one hooked in
-              fetchingMap[dataNumber].forEach(cb => cb(obj))
+              fetchingMap[dataNumber].forEach(cb2 => cb2(obj2))
             }
 
             delete fetchingMap[dataNumber]
+          })
+          .catch((err) => {
+            if (cb) {
+              cb(err)
+            }
           })
       } catch (e) {
         console.error(e)
@@ -77,9 +106,9 @@ function DataManager(datastore) {
     fetchRange: (rang) => {
       const [start, stop] = rang
       const iterator = range(stop - start, start)
-
-      console.log(iterator)
-      iterator.forEach(retObj.fetchData)
+      iterator.forEach((index) => {
+        retObj.fetchData(index, null)
+      })
     },
     fetchList: (cb) => {
       let callback = cb
@@ -95,9 +124,8 @@ function DataManager(datastore) {
           dataNumberList = []
           dataList.forEach((obj) => {
             dataNumberList.push(obj.dn)
+            s3keys[obj.dn] = obj.si
           })
-          console.log(dataNumberList)
-          console.log(dataList)
           callback(null, list.length)
         })
         .catch(err => callback(err))
@@ -116,8 +144,19 @@ function DataManager(datastore) {
         }
         fetchingMap[dataNumber].push(callback)
         // dont fetch because we are already fetching it.
+        console.log('NOT FETCHING BECAUSE ALREADY FETCH IN PROGRESS')
         return null
       }
+
+      // FOR NOW we are going to assume that if a dataNumber is to be
+      // fetched, that the client already has that dataNumber in the dataNumberList
+      // however, in the future, we should implement checking to see if it is there.
+      // if it is NOT there, we need to first find the s3 key for that data number, and
+      // then fetch it!
+
+      const s3key = s3keys[dataNumber]
+
+      retObj.fetchData(null, { key: s3key, dn: dataNumber, cb: callback })
     },
     getDataNumbers: () => [...dataNumberList],
   }
