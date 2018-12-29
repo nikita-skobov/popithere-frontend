183d6f3da8bc00bc7c61baa4c025c65d1afd5c99
nikita-skobov <nskobov.websites@gmail.com>
2018-12-28 22:59:08 -0600

    adds base fetch endpoint for s3 fetches

--- frontend/src/customConfig.js
+++ frontend/src/customConfig.js
@@ -21,6 +21,7 @@ exports.defaultButtons = [
   'options',
 ]
 
+exports.dataFetchBase = 'https://api.popithere.com/'
 exports.listDataEndpoint = 'https://api.popithere.com/list/data'
 exports.socketEndpoint = 'https://sockets.popithere.com'
 exports.loginEndpoint = 'https://5wwuqdullg.execute-api.us-east-1.amazonaws.com/staging/issue/login'
