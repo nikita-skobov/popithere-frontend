e9d30a459473a7f2bd788eaf37b9afb9f906b5a9
nikita-skobov <nskobov.websites@gmail.com>
2019-01-05 14:24:54 -0600

    changes FirstTime to use a controlled carousel

--- frontend/src/components/FirstTime.js
+++ frontend/src/components/FirstTime.js
@@ -2,7 +2,11 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 import {
-  UncontrolledCarousel,
+  Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselCaption
 } from 'reactstrap'
 
 import { firstTimeItems } from '../customConfig'
@@ -16,6 +20,15 @@ export default class FirstTime extends Component {
 
     this.brain.store('FirstTime', this)
 
+    this.state = {
+      activeIndex: 0,
+    }
+
+    this.next = this.next.bind(this)
+    this.previous = this.previous.bind(this)
+    this.onExiting = this.onExiting.bind(this)
+    this.onExited = this.onExited.bind(this)
+
     this.handleButton = this.handleButton.bind(this)
 
     this.items = firstTimeItems.latest
@@ -25,9 +38,53 @@ export default class FirstTime extends Component {
     e.preventDefault()
   }
 
+  onExiting() {
+    this.animating = true
+  }
+
+  onExited() {
+    this.animating = false
+  }
+
+  next() {
+    if (this.animating) return
+    const { activeIndex } = this.state
+    const nextIndex = activeIndex === this.items.length - 1 ? 0 : activeIndex + 1
+    this.setState({ activeIndex: nextIndex })
+  }
+
+  previous() {
+    if (this.animating) return
+    const { activeIndex } = this.state
+    const nextIndex = activeIndex === 0 ? this.items.length - 1 : activeIndex - 1
+    this.setState({ activeIndex: nextIndex })
+  }
+
   render() {
+    const { activeIndex } = this.state
+    const slides = this.items.map((item) => {
+      return (
+        <CarouselItem
+          onExiting={this.onExiting}
+          onExited={this.onExited}
+          key={item.src}
+        >
+          <img src={item.src} alt={item.altText} />
+          <CarouselCaption captionText={item.caption} captionHeader={item.header} />
+        </CarouselItem>
+      )
+    })
+
     return (
-      <UncontrolledCarousel autoPlay={false} items={this.items} />
+      <Carousel
+        activeIndex={activeIndex}
+        next={this.next}
+        previous={this.previous}
+      >
+        {slides}
+        <CarouselControl direction="prev" directionText="Previous" onClickHandler={this.previous} />
+        <CarouselControl direction="next" directionText="Next" onClickHandler={this.next} />
+      </Carousel>
     )
   }
 }
