721580453e6550c5e79d8fd2524870cda1e2d2f7
nikita-skobov <nskobov.websites@gmail.com>
2019-01-09 14:55:52 -0600

    adds button to log in with patreon

--- frontend/src/components/PatreonBenefits.js
+++ frontend/src/components/PatreonBenefits.js
@@ -11,9 +11,10 @@ import {
   Card,
   CardTitle,
   CardText,
+  Button,
 } from 'reactstrap'
 
-import { benefitTiers } from '../customConfig'
+import { benefitTiers, patreonEndpoint, patreonClientId } from '../customConfig'
 
 const has = Object.prototype.hasOwnProperty
 
@@ -54,6 +55,14 @@ export default class PatreonBenefits extends Component {
     this.brain.store('PatreonBenefits', this)
 
     this.handleButton = this.handleButton.bind(this)
+
+    const username = tm.getClaim('id')
+
+    if (!username) {
+      this.patreonAuthorization = '#'
+    } else {
+      this.patreonAuthorization = `https://www.patreon.com/oauth2/authorize?state=${username}&response_type=code&scope=identity%20campaigns.members&client_id=${patreonClientId}&redirect_uri=${patreonEndpoint}`
+    }
   }
 
   handleButton(e) {
@@ -89,6 +98,15 @@ export default class PatreonBenefits extends Component {
             <h3 className="text-center">You are using this site without a token. You cannot link your patreon account until you aquire a token. Please try refreshing the page. If this issue persists, please contact equilateralllc@gmail.com</h3>
           </Row>
         )}
+        <Row>
+          <h5 className="text-center">If you are a patron, you have access to benefits on this site. All you need to do is click the {'"Log in with Patreon"'} button, which will unlock your benefits for this specific device. If you have multiple devices, you just need to click this button again on your other devices.</h5>
+        </Row>
+        <Row>
+          <a className="ma" href={this.patreonAuthorization}>
+            <Button className="btn-patreon">Log in with Patreon</Button>
+          </a>
+        </Row>
+        <Row />
         <Row>
           {makeCard('notier', 'No Tier')}
           {makeCard('basic', 'Basic Tier')}
