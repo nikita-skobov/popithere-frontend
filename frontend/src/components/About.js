5ddbbb57785c6091f90150847eae02ce26a00d0e
nikita-skobov <nskobov.websites@gmail.com>
2019-01-11 13:13:46 -0600

    adds styling to about componenet

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -5,14 +5,27 @@ import {
   DropdownToggle,
   DropdownMenu,
   DropdownItem,
+  Row,
+  Col,
 } from 'reactstrap'
 
 import { defaultButtons } from '../customConfig'
 
 export default function About() {
   return (
-    <div>
-      dsadsad
-    </div>
+    <Col fluid>
+      <Row>
+        <h1 className="ma mthalfem mbhalfem">PopItHere!</h1>
+      </Row>
+      <Row className="cool-bottom-line">
+        <h6 className="ml1em mr1em pb1em">
+          PopItHere is part game, part collaborative art piece.
+           Users can create their own gifs/images (called PopIts),
+           and place them down anywhere on a square canvas. When
+           they place a PopIt, it covers up anything that was under
+           it, which makes the canvas change very quickly.
+        </h6>
+      </Row>
+    </Col>
   )
 }
